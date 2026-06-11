9f28e5293b6b3315eb154aa11d1b436a172acfd1
IzumiKonata <izumiikonatta@gmail.com>
2026-06-11 20:06:07 +0800

    bump version

--- VOCALOIDPatcher/VOCALOIDPatcher/Patcher.cs
+++ VOCALOIDPatcher/VOCALOIDPatcher/Patcher.cs
@@ -34,7 +34,7 @@ public static class Patcher
 
     public static bool VstPluginMode;
 
-    public static string Version => "2.2.2";
+    public static string Version => "2.2.3";
 
 #pragma warning disable CA2255
     [ModuleInitializer]
